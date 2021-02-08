eda95f98dcdb5a12317fd236ceec353de0b0dd46
Bach Tran <bach.tran@revature.com>
2021-02-08 11:42:45 -0600

    Add HelloWorld.java example

---
+++ my-first-application/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	
+	// You can write comments like this, which are for programmers to read, but are ignored by the compiler
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
